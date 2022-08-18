42a7a7c01f1fdcc459c1bbaa46194f9febd19fc0
Yassine Sen <0139ne@gmail.com>
2022-08-18 01:07:45 +0100

    Add sample for debugging visual behaviour tree

---
+++ test/debuggable_tree/node.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var exec_tree = preload("res://new_v_behaviour_tree_resource.vbtree")
+@onready var tree_holder = $VisualBTree as BehaviourTreeRemoteTreeHolder
+
+func _ready():
+	tree_holder.vbehaviour_tree = exec_tree
+
+func _process(_delta):
+	exec_tree.behaviour_tree.execute_tree()
